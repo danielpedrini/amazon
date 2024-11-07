ba1dddb4717a45724fbeca9ce548200b4e4759ac
Daniel Pedrini <daniel.pedrini@gmail.com>
2024-11-07 11:52:29 -0300

    CloudFlare configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
 
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
 
   integrations: [tailwind(), react()]
 });
