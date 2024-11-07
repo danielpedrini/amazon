4dd731b9113874063d7aa50c4e809b2b117038d0
Daniel Pedrini <daniel.pedrini@gmail.com>
2024-11-07 12:09:05 -0300

    env fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-if (typeof window === 'undefined') {
-  import('dotenv/config');
-}
 
-const supabaseUrl  = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl  = 'https://qwphxdvvybbzjdfnaqlf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3cGh4ZHZ2eWJiempkZm5hcWxmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDIzNTE2NywiZXhwIjoyMDQ1ODExMTY3fQ.4NJW_IUmw3BS-do4CCJ6Q69Mv3Yb1qEc5753RvbgVSw';
 console.log(supabaseUrl, supabaseKey)
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("SUPABASE_URL e SUPABASE_KEY precisam estar definidas nas variáveis de ambiente.");
